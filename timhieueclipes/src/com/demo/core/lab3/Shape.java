84e4f59ba9552cbebfb155e79e6a65e04310c8fa
sang <doanvietsang2088@gmail.com>
2020-07-24 19:45:27 +0700

    1

--- timhieueclipes/src/com/demo/core/lab3/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.demo.core.lab3;
-
-public abstract class Shape {
-String color;
-public abstract double getArea();
-public abstract String toString();
-}
--- timhieueclipes/src/com/demo/core/lab6/QLTT.java
+++
@@ -1,7 +0,0 @@
-package com.demo.core.lab6;
-
-public class QLTT {
-String CMND;
-String name;
-
-}
